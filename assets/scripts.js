66bf440edd52428d09795578fbeaba6d06634d6c
bielTsilva <gelogabriel@gmail.com>
2022-05-16 02:02:46 -0300

    correção de problema na troca de cor

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,18 +6,31 @@ var currentNumber = 0;
 btnIncrement.addEventListener("click", increment);
 btnDecrement.addEventListener("click", decrement);
 
+
+
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
-        if(currentNumber >= 10){
-            currentNumberWrapper.style.color = 'white'
-        }
+    if(currentNumber >= 10){
+        currentNumberWrapper.style.color = 'white'
+        console.log('white')
+    }
+    else if(currentNumber > 0 && currentNumber <10 ){
+        currentNumberWrapper.style.color = 'black'
+        console.log('black')
+    }
 }
 
 function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
-        if(currentNumber < 0){
-            currentNumberWrapper.style.color = 'red'
-        }
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red'
+        console.log('red')
+    }
+    else if(currentNumber > 0 && currentNumber <10 ){
+        currentNumberWrapper.style.color = 'black'
+        console.log('black')
+    }
 }
+
